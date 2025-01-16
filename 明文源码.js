d846611af20201273dfa6ad54d88a5e401932444
lszghcpp <158157016+lszghcpp@users.noreply.github.com>
2025-01-16 15:38:15 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = '1339d12e-327f-4dc7-b8bf-c0e603de158f';
+let userID = '0d9f9541-220d-4d97-a1eb-7202d13b841e';
 
 // 生成配置文件的 Cloudflare 优选 IP (www.gov.se   speed.cloudflare.com等使用Cloudflare CDN的网站也可以)
 const bestCFIP = "www.visa.com.hk"
